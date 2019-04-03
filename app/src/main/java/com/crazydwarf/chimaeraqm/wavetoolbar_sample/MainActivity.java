78ba0a2fc53ea771bf1faa4884e1273ed82aa9fc
chimaeraqm <chimaeraqm@126.com>
2019-04-03 16:14:14 +0800

    library_wavetoolbar v1.0
    --[20190403] Separated library wavetoolbar from original project. Add sample module to this project.

---
+++ app/src/main/java/com/crazydwarf/chimaeraqm/wavetoolbar_sample/MainActivity.java
@@ -0,0 +1,52 @@
+package com.crazydwarf.chimaeraqm.wavetoolbar_sample;
+
+import android.app.ActivityManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.crazydwarf.chimaeraqm.wavetoolbar.WaveToolbar;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    /**
+     * exitTime represent the gap between two EXIT button clicks
+     */
+    private long exitTime = 0;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WaveToolbar toolBar = findViewById(R.id.top_menu);
+        setSupportActionBar(toolBar);
+        toolBar.setMenuIconClickListener(new WaveToolbar.MenuIconClickListener() {
+            @Override
+            public void OnClick(View view) {
+                Toast.makeText(getApplicationContext(), "You just clicked MENU button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        toolBar.setBackIconClickListener(new WaveToolbar.BackIconClickListener() {
+            @Override
+            public void OnClick() {
+                //Make two continuous click to exit the programme
+                activityExit();
+            }
+        });
+    }
+
+    public void activityExit() {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(getApplicationContext(), "One more click to exit the programme", Toast.LENGTH_LONG).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            Toast.makeText(getApplicationContext(), "Made two continuous click to exit the programme", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+}
--- wavetoolbar/src/androidTest/java/com/crazydwarf/chimaeraqm/wavetoolbar/ExampleInstrumentedTest.java
+++ wavetoolbar/src/androidTest/java/com/crazydwarf/chimaeraqm/wavetoolbar/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.crazydwarf.chimaeraqm.wavetoolbar", appContext.getPackageName());
+        assertEquals("com.crazydwarf.chimaeraqm.wavetoolbar.test", appContext.getPackageName());
     }
 }
